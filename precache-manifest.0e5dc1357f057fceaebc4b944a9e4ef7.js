a0fc754f755db7e7dc961073e76f0a1cc0f5a699
Vaidee <vaideeswaran@agentz.ai>
2020-04-28 22:58:16 +0530

    Updates

---
+++ precache-manifest.0e5dc1357f057fceaebc4b944a9e4ef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85252ac6e22dff14c6b0ddcb0ec6cf54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da8258daad9a33f5d5fa",
+    "url": "/static/css/main.6e3a1ed2.chunk.css"
+  },
+  {
+    "revision": "bc45b134f91e31fdbcd8",
+    "url": "/static/js/2.c295bf7e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c295bf7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da8258daad9a33f5d5fa",
+    "url": "/static/js/main.23be87cf.chunk.js"
+  },
+  {
+    "revision": "b8abd67c8094511b7b88",
+    "url": "/static/js/runtime-main.d135084e.js"
+  }
+]);
