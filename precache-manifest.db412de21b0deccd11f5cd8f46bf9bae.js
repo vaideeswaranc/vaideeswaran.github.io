88b9b11e2744198d2b6ead62555610e993f5fdfd
Vaidee <vaideeswaran@agentz.ai>
2020-04-28 23:04:08 +0530

    Updates

---
+++ precache-manifest.db412de21b0deccd11f5cd8f46bf9bae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0dd890f5273b96bffedf060f365b319",
+    "url": "/vaideeswaran.github.io/index.html"
+  },
+  {
+    "revision": "f97247c6dcc9ce26e3c7",
+    "url": "/vaideeswaran.github.io/static/css/main.6e3a1ed2.chunk.css"
+  },
+  {
+    "revision": "bc45b134f91e31fdbcd8",
+    "url": "/vaideeswaran.github.io/static/js/2.c295bf7e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/vaideeswaran.github.io/static/js/2.c295bf7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f97247c6dcc9ce26e3c7",
+    "url": "/vaideeswaran.github.io/static/js/main.00709290.chunk.js"
+  },
+  {
+    "revision": "70215d7bd3f5ab6bea2e",
+    "url": "/vaideeswaran.github.io/static/js/runtime-main.b8b8ce1b.js"
+  }
+]);
